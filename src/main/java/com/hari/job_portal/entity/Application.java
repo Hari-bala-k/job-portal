816d1a058fd19a6a6cc5707d04068686256cab23
Hari <techiehari18hk@gmail.com>
2026-06-18 22:25:43 +0530

    Add entity relationships between user company job and application

--- src/main/java/com/hari/job_portal/entity/Application.java
+++ src/main/java/com/hari/job_portal/entity/Application.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,4 +22,12 @@ public class Application {
      private Long id;
      private String status;
      private  LocalDate applicationDate;
+     
+     @ManyToOne
+     @JoinColumn(name = "user_id")
+     private User user;
+     
+     @ManyToOne
+     @JoinColumn(name = "job_id")
+     private Job job;
 }
--- src/main/java/com/hari/job_portal/entity/Company.java
+++ src/main/java/com/hari/job_portal/entity/Company.java
@@ -1,8 +1,12 @@
 package com.hari.job_portal.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,11 +17,14 @@ import lombok.Setter;
 @Table(name = "companies")
 public class Company {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
     private String description;
     private String location;
     private String website;
+    
+    @OneToMany(mappedBy = "company")
+    private List<Job> jobs;
 }
