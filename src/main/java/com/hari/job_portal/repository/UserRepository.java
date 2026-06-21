dedbc018c7e07959df1deea056e2302055c6b116
Hari <techiehari18hk@gmail.com>
2026-06-21 22:41:00 +0530

    Complete service layer

--- src/main/java/com/hari/job_portal/repository/UserRepository.java
+++ src/main/java/com/hari/job_portal/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.hari.job_portal.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+       boolean existsByEmail(String email);
 }
