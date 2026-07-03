414485e07e72ea2190ea32c78a068eeea947cc93
Hari <techiehari18hk@gmail.com>
2026-07-03 21:52:22 +0530

    Added ApplicationDTO

--- src/main/java/com/hari/job_portal/dto/ApplyRequestDTO.java
+++ src/main/java/com/hari/job_portal/dto/ApplyRequestDTO.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class ApplicationRequestDTO {
+public class ApplyRequestDTO {
 
     @NotNull(message = "User ID is required")
     private Long userId;
